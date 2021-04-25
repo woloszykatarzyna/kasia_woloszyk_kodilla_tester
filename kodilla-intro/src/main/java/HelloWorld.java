29eb359f5aecdc6855cf3f7993a07380db42542e
Kasia Wołoszyk <woloszykatarzyna@gmail.com>
2021-04-25 12:33:02 +0200

    Hello World

---
+++ kodilla-intro/src/main/java/HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("Kasia");
+        System.out.println(30);
+        System.out.println("W");
+    }
+}
+
---
+++ src/main/java/FirstClass.java
@@ -0,0 +1,5 @@
+public class FirstClass {
+    public static void main(String[] args) {         // [2]
+        System.out.println("Hello from FirstClass!"); // [3]
+    }
+}
