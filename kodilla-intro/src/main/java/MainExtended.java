84f30dd2294a96707539b44f44bbeedbadf77f52
Kasia Wołoszyk <woloszykatarzyna@gmail.com>
2021-04-25 13:17:17 +0200

    Main Extended

---
+++ kodilla-intro/src/main/java/MainExtended.java
@@ -0,0 +1,16 @@
+public class MainExtended {
+    public static void main(String[] args) {
+        String firstBug = "Missing screen";
+        String secondBug = "Missing button";
+        String thirdBug = "Wrong error message";
+        String fourthBug = "Missing error message";
+        String fifthBug = "Incorrect validation";
+        String sixthBug = "Incorrect view";
+        System.out.println(firstBug);
+        System.out.println(secondBug);
+        System.out.println(thirdBug);
+        System.out.println(fourthBug);
+        System.out.println(fifthBug);
+        System.out.println(sixthBug);
+    }
+}
