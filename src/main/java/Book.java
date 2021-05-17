3e71bc783ce783781e651bd5189be9f2b8474de3
Kasia Wołoszyk <woloszykatarzyna@gmail.com>
2021-05-17 20:44:30 +0200

    Zmienne i metody statyczne - fix

--- src/main/java/Book.java
+++ src/main/java/Book.java
@@ -11,12 +11,12 @@ public class Book {
         this.title = title;
     }
 
-    public static Object of(String author, String title) {
+    public static Book of(String author, String title) {
         Book book = new Book(author, title);
         return book;
     }
     public static void main(String[] args) {
-        Book tolkien = (Book) Book.of("Tolkien", "Hobbit");
+        Book tolkien = Book.of("Tolkien", "Hobbit");
         System.out.println(tolkien.author + " "+ tolkien.title);
     }
 }
