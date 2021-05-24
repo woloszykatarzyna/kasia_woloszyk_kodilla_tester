63b552f73ab1c3307c5127a24d6db618f3895788
Kasia Wołoszyk <woloszykatarzyna@gmail.com>
2021-05-24 20:50:28 +0200

    Zadanie: dziedziczenie i klasy abstrakcyjne - fix

--- kodilla-basic-tests/src/main/java/com/kodilla/abstracts/homework/ApplicationJob.java
+++ kodilla-basic-tests/src/main/java/com/kodilla/abstracts/homework/ApplicationJob.java
@@ -4,17 +4,17 @@ public class ApplicationJob {
     public static void main(String[] args) {
         Job hydraulik = new Hydraulik();
 
-        Person processorHydraulik = new Person("Jan", 50);
+        Person processorHydraulik = new Person("Jan", 50, "Hydraulik");
         processorHydraulik.process(hydraulik);
 
         Job drwal = new Drwal();
 
-        Person processorDrwal = new Person("Tadeusz", 20);
+        Person processorDrwal = new Person("Tadeusz", 20, "Drwal");
         processorDrwal.process(drwal);
 
         Job elektryk = new Elektryk();
 
-        Person processorElektryk = new Person("Kazimierz", 30);
+        Person processorElektryk = new Person("Kazimierz", 30, "Elektryk");
         processorElektryk.process(elektryk);
     }
 }
--- kodilla-basic-tests/src/main/java/com/kodilla/abstracts/homework/Person.java
+++ kodilla-basic-tests/src/main/java/com/kodilla/abstracts/homework/Person.java
@@ -3,12 +3,14 @@ package com.kodilla.abstracts.homework;
 public class Person {
     String firstName;
     int age;
-    public Person(String firstName, int age){
+    String job;
+    public Person(String firstName, int age, String job){
         this.firstName = firstName;
         this.age = age;
+        this.job = job;
     }
 
     public void process(Job person) {
-        System.out.println(firstName + " " + age + " " + person.jobResponsibilities());
+        System.out.println(firstName + " " + age + " " + job + " " + person.jobResponsibilities());
     }
 }
